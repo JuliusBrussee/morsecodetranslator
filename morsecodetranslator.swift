376ceb708f425d4c8f9ae9854d8d090e6dcda98d
Julius Brussee <104168679+JuliusBrussee@users.noreply.github.com>
2024-10-19 14:10:28 +0200

    Update morsecodetranslator.swift

--- morsecodetranslator.swift
+++ morsecodetranslator.swift
@@ -1,81 +1,81 @@
-var englishText = "this is a secret message"
+// Usage
+let englishText = "this is a secret message"
+let secretMessage = ".... --- .-- -.. -.--   .--. .- .-. - -. . .-."
 
-var secretMessage = ".... --- .-- -.. -.--   .--. .- .-. - -. . .-."
 
-// Add your code below 🤫
-var letterToMorse = [
-  "a": ".-",
-  "b": "-...",
-  "c": "-.-.",
-  "d": "-..",
-  "e": ".",
-  "f": "..-.",
-  "g": "--.",
-  "h": "....",
-  "i": "..",
-  "j": ".---",
-  "k": "-.-",
-  "l": ".-..",
-  "m": "--",
-  "n": "-.",
-  "o": "---",
-  "p": ".--.",
-  "q": "--.-",
-  "r": ".-.",
-  "s": "...",
-  "t": "-",
-  "u": "..-",
-  "v": "...-",
-  "w": ".--",
-  "x": "-..-",
-  "y": "-.--",
-  "z": "--.."
-]
-var morseText = ""
-
-for element in englishText {
-  if let morseChar = letterToMorse["\(element)"] {
-    morseText.append("\(morseChar) ")
-  }
-  else {
-    morseText.append("   ")
-  }
-}
-print(morseText)
-
-var decodedMessage = ""
-var morseCodeArray = [String]()
-var currMorse = ""
-
-for char in secretMessage {
-  if char != " " {
-    currMorse.append(char)
-  } else {
-    switch currMorse {
-      case "":
-        currMorse += " "
-      case " ":
-        morseCodeArray.append(" ")
-        currMorse = ""
-      default:
-        morseCodeArray.append(currMorse)
-        currMorse = ""
+func encodeToMorse(englishText: String) -> String {
+    let letterToMorse = [
+        "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.",
+        "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..",
+        "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.",
+        "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-",
+        "y": "-.--", "z": "--.."
+    ]
+    
+    var morseText = ""
+    
+    for element in englishText.lowercased() {
+        if let morseChar = letterToMorse[String(element)] {
+            morseText.append("\(morseChar) ")
+        } else {
+            morseText.append("   ")
+        }
     }
-  }
+    
+    return morseText
 }
-morseCodeArray.append(currMorse)
-print(morseCodeArray)
 
-var morseToLetter: [String: String] = [:]
-for (letter, morseChar) in letterToMorse {
-  morseToLetter[morseChar] = letter
+func decodeFromMorse(secretMessage: String) -> String {
+    let letterToMorse = [
+        "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.",
+        "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..",
+        "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.",
+        "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-",
+        "y": "-.--", "z": "--.."
+    ]
+    
+    var morseToLetter: [String: String] = [:]
+    for (letter, morseChar) in letterToMorse {
+        morseToLetter[morseChar] = letter
+    }
+    
+    var decodedMessage = ""
+    var morseCodeArray = [String]()
+    var currMorse = ""
+    
+    for char in secretMessage {
+        if char != " " {
+            currMorse.append(char)
+        } else {
+            switch currMorse {
+                case "":
+                    currMorse += " "
+                case " ":
+                    morseCodeArray.append(" ")
+                    currMorse = ""
+                default:
+                    morseCodeArray.append(currMorse)
+                    currMorse = ""
+            }
+        }
+    }
+    morseCodeArray.append(currMorse)
+    
+    for morseValue in morseCodeArray {
+        if let letterChar = morseToLetter[morseValue] {
+            decodedMessage += letterChar
+        } else {
+            decodedMessage += " "
+        }
+    }
+    
+    return decodedMessage
 }
-print(morseToLetter)
-for morseValue in morseCodeArray {
-  if let letterChar = morseToLetter[morseValue] {
-    decodedMessage += letterChar
-  } else {
-    decodedMessage += " "
-  }
-}
-print(decodedMessage)
+
+// Encode to Morse
+let morseCode = encodeToMorse(englishText: englishText)
+print("Encoded Morse: \(morseCode)")
+
+// Decode from Morse
+let decodedMessage = decodeFromMorse(secretMessage: secretMessage)
+print("Decoded Message: \(decodedMessage)")
